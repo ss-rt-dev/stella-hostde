4914750a62f867693fdb96a21524743814684bc0
RT Dev <conzzymusic@gmail.com>
2026-08-01 00:31:23 +0200

    Scroll-Pfeil-Buttons entfernen

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,13 +2,11 @@
 
 import { SessionProvider } from "next-auth/react";
 import { CookieBanner } from "@/components/CookieBanner";
-import { ScrollControls } from "@/components/ScrollControls";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       {children}
-      <ScrollControls />
       <CookieBanner />
     </SessionProvider>
   );
