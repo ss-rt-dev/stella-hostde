60868ebbaefba78e09fe1b73ae01fff77def512b
RT Dev <conzzymusic@gmail.com>
2026-08-01 15:36:36 +0200

    Cookie-Banner von Landing Page entfernen

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,13 +1,7 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import { CookieBanner } from "@/components/CookieBanner";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <SessionProvider>
-      {children}
-      <CookieBanner />
-    </SessionProvider>
-  );
+  return <SessionProvider>{children}</SessionProvider>;
 }
