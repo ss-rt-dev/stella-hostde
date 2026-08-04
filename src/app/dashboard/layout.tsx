d54dfc3c55fcdc7ab64f1685b1d153333eefa42e
RT Dev <conzzymusic@gmail.com>
2026-08-04 23:49:02 +0200

    Fix dashboard layout: fehlende } in className Template

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -35,7 +35,9 @@ export default async function DashboardLayout({
   return (
     <div className="min-h-screen bg-[#0a0a0c] text-zinc-100">
       <DashboardNav user={session.user as any} />
-      <main className={`lg:pl-[240px] ${impersonating ? "pt-10" : ""`}>
+      <main
+        className={`lg:pl-[240px] ${impersonating ? "pt-10" : ""}`}
+      >
         <div className="mx-auto max-w-[1100px] px-4 py-5 pb-20 lg:px-6 lg:py-6 lg:pb-8">
           {showMaintenance ? (
             <MaintenanceScreen />
